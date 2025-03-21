b3733590cb7d7f459a4ed8829eb2bca60e6e1af7
Donno2001 <gayosso.angel.1bv@gmail.com>
2025-03-20 23:08:02 -0600

    htpps porfaaaaaaa

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\User;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -15,6 +16,12 @@ class AppServiceProvider extends ServiceProvider
     {
     //    
     }
+    public function boot()
+{
+    if (env('APP_ENV') === 'production') {
+        URL::forceScheme('https');
+    }
+}
 
   /**
      * Bootstrap any application services.
